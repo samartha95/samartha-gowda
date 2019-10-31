f754cd4f33d7cb3a88dfcb16db3fc052fa172446
samartha95 <57202238+samartha95@users.noreply.github.com>
2019-10-31 09:42:36 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,17 @@
+package com.primitivetypecasting;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sum(10,20);}
+		
+	public static void sum(int a,int b) {
+		System.out.println(a+b);}
+	public static int sum(int a,int b) {
+		return (a+b);
+	}
+
+	}
+
+
