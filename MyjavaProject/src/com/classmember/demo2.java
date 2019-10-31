b8c3cfec6b1a9fd76f01f8d1eadeb22493b5ac42
e7240 <e7240@dfll>
2019-10-31 16:55:51 +0530

    ok

---
+++ MyjavaProject/src/com/classmember/demo2.java
@@ -0,0 +1,16 @@
+package com.classmember;
+
+public class demo2 {
+	static int i =10;
+
+	public static void main(String[] args) {
+		
+		System.out.println(i);
+		i=30;
+		System.out.println(demo2.i);
+		
+
+
+	}
+
+}
---
+++ MyjavaProject/src/com/classmember/demo3.java
@@ -0,0 +1,12 @@
+package com.classmember;
+
+public class demo3 {
+
+	public static void main(String[] args) {
+		System.out.println(demo2.i);
+		demo2.i=20;
+		System.out.println(demo2.i);
+
+	}
+
+}
