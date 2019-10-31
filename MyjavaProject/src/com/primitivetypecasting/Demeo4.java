6089b45847384421d844ed88564fa0767e9e8158
e7240 <e7240@dfll>
2019-10-31 11:26:28 +0530

    samartha

---
+++ MyjavaProject/src/com/primitivetypecasting/Demeo4.java
@@ -0,0 +1,17 @@
+package com.primitivetypecasting;
+
+public class Demeo4 {
+
+	public static void main(String[] args) {
+		int q[]= {10,20,30};
+		for(int index=0;index<(q.length);index++)
+			System.out.println(q[index]);
+		for(int i=0;i<(q.length);i++)
+			q[i]=q[i]+10;
+		for(int index=0;index<(q.length);index++)
+			System.out.println(q[index]);
+		
+
+	}
+
+}
